2ae89e00b58a26a24f7837b18299afcff51ced9e
Toru Murakami <mrkm.toru@gmail.com>
2018-01-24 00:08:21 +0900

    now app can add numbers on browser!

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const add = require('./adder.js');
 
 class ViewManager {
+
 	connectEventHundlers() {
-
+		document.getElementById('form-numbers')
+		.addEventListener('submit', this.onSubmit);
 	}
-	onSubmit(event) {
 
+	onSubmit(event) {
+		event.preventDefault();
+		let num1 = document.getElementById('input-num1').value;
+		let num2 = document.getElementById('input-num2').value;
+		num1 = parseInt(num1, 10);
+		num2 = parseInt(num2, 10);
+		const sum = add(num1, num2);
+		alert('合計は' + num1 + '+' + num2 + '=' + sum + 'です');
 	}
 }
 
 const viewManager = new ViewManager();
-viewManager.connectEventHandlers();
+viewManager.connectEventHundlers();
--- bundle.js
+++ bundle.js
@@ -7,5 +7,24 @@ module.exports = add;
 },{}],2:[function(require,module,exports){
 const add = require('./adder.js');
 
-console.log(add(5, 6));
+class ViewManager {
+
+	connectEventHundlers() {
+		document.getElementById('form-numbers')
+		.addEventListener('submit', this.onSubmit);
+	}
+
+	onSubmit(event) {
+		event.preventDefault();
+		let num1 = document.getElementById('input-num1').value;
+		let num2 = document.getElementById('input-num2').value;
+		num1 = parseInt(num1, 10);
+		num2 = parseInt(num2, 10);
+		const sum = add(num1, num2);
+		alert('合計は' + num1 + '+' + num2 + '=' + sum + 'です');
+	}
+}
+
+const viewManager = new ViewManager();
+viewManager.connectEventHundlers();
 },{"./adder.js":1}]},{},[2]);
