81186784faa912c7c8167382dcd4b55bbc46586e
Toru Murakami <mrkm.toru@gmail.com>
2018-01-24 19:43:24 +0900

    Lesson3_24 cleared.

--- server/server.js
+++ server/server.js
@@ -5,21 +5,35 @@ const fs = require('fs');
 const server = http.createServer(function(request, response) {
 
   const path = url.parse(request.url).pathname;
-  console.log(path);  // for debug
-  const filePath = '.' + path;
+  let filePath = './client' + path;
+  console.log(filePath);  // for debug
 
   fs.exists(filePath, doesExist => {
     if (!doesExist) {
       response.setHeader('Content-Type', 'text/html; charset=UTF-8');
       response.statusCode = 404;
-      response.end(`ERROR CODE 404 - ファイル(${path})が存在しませんでした。`.toString('UTF-8'));
+      response.end(`ERROR CODE 404 - ファイル('${path}')が存在しませんでした。`.toString('UTF-8'));
     } else {
-      fs.readFile(filePath, (err, data) => {
-        if(err) {
+      fs.stat(filePath, (err, fileInfo) => {
+        if (err) {
+          response.setHeader('Content-Type', 'text/html; charset=UTF-8');
           response.statusCode = 500;
           response.end(`Internal Server Error!! \n ${err}`.toString('UTF-8'));
+        } else {
+          if (fileInfo.isDirectory()) {
+            filePath += '/index.html';
+          }
+          fs.readFile(filePath, (err, data) => {
+            if(err) {
+              response.setHeader('Content-Type', 'text/html; charset=UTF-8');
+              response.statusCode = 500;
+              response.end(`Internal Server Error!! \n ${err}`.toString('UTF-8'));
+            }
+            response.setHeader('Content-Type', 'text/html; charset=UTF-8');
+            response.statusCode = 200;
+            response.end(data.toString('UTF-8'));
+          });
         }
-        response.end(data.toString('UTF-8'));
       });
     }
   });
